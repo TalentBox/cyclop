9e4fe15dabc031c3f71896a8c0372269189b42a5
Jonathan Tron <jonathan.tron@thetalentbox.com>
2011-08-11 21:28:01 +0200

    Add RUBY_VERSION to db name used for spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ Dir[File.expand_path("../support/**/*.rb", __FILE__)].each{|f| require f}
 RSpec.configure do |config|
   config.before(:all) do
     logger = Logger.new "test.log"
-    Cyclop.db = Mongo::Connection.new("localhost", nil, :logger => logger).db "cyclop_test"
+    Cyclop.db = Mongo::Connection.new("localhost", nil, :logger => logger).db "cyclop_test#{RUBY_VERSION}"
   end
   config.before do
     Cyclop.db.collections.each(&:remove)
